dd905b8cd4c05c62a3297e973c4fefe6c50005aa
unknown <raflisilehu20@gmail.com>
2024-08-12 18:44:22 +0700

    update sitemap.xml

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://ums-iid.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://ums-iid.com/registration',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
