65c34314d13c0dd0e4d1215808144f2c876b24cb
unknown <raflisilehu20@gmail.com>
2024-08-12 20:16:25 +0700

    update sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://ums-iid.com',
+      url: 'https://ums-iid.com/',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
