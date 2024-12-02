513e04309809571582597ec375719418aecd8755
Josumaru <jokosupriyanto1712@gmail.com>
2024-12-02 17:17:46 +0700

    feat: redirect registration url to root

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,10 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 export function middleware(request: NextRequest) {
-  // if (request.nextUrl.pathname.startsWith('/registration-innovation-talk')) {
-  //   return NextResponse.redirect(new URL('/', request.url))
-  // }
+  if (request.nextUrl.pathname.startsWith('/registration-innovation-talk')) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+  if (request.nextUrl.pathname.startsWith('/registration-competition')) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
 }
